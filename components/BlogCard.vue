7705f2025b31ae2f84254caafb6867ebb90fcaa0
YasmineSan <yasmine-sanchez@proton.me>
2025-02-14 19:49:09 +0100

    Feat: implement BlogCard component and refactor index.vue for improved structure

---
+++ components/BlogCard.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { NuxtImg } from '#components'
+
+defineProps({
+  title: String,
+  category: String,
+  date: String,
+  description: String,
+  imageSrc: String,
+  avatarSrc: String,
+  author: String,
+})
+
+const isHovered = ref(false)
+</script>
+
+<template>
+  <div
+    class="relative w-full max-w-card-lg-w xs:max-w-card-sm-w"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <!-- Ombre dynamique au hover -->
+    <div
+      :class="[
+        'absolute w-full h-full bg-dark rounded-xl left-2 top-2 z-0 transition-all duration-300',
+        isHovered ? 'translate-x-1 translate-y-1 scale-102 opacity-80' : '',
+      ]"
+    ></div>
+
+    <!-- Blog Card -->
+    <section
+      class="relative bg-white w-full rounded-xl shadow-lg p-6 z-10 border border-dark group transition-shadow duration-300 group-hover:shadow-2xl"
+    >
+      <article class="flex flex-col items-center">
+        <NuxtImg
+          :src="imageSrc"
+          alt="Article image"
+          class="w-img-lg-w h-img-lg-h xs:w-img-sm-w xs:h-img-sm-h object-cover mb-6 rounded-lg"
+        />
+      </article>
+
+      <span
+        class="bg-primary text-dark px-3 py-1 rounded-md text-sm font-semibold"
+      >
+        {{ category }}
+      </span>
+
+      <p class="text-sm py-2">{{ date }}</p>
+
+      <article>
+        <h1
+          class="text-lg font-extrabold cursor-pointer group-hover:text-primary transition-colors duration-75"
+        >
+          {{ title }}
+        </h1>
+        <p class="text-sm text-gray py-2">
+          {{ description }}
+        </p>
+      </article>
+
+      <article class="flex items-end">
+        <img
+          :src="avatarSrc"
+          alt="avatar of {{ author }}"
+          class="size-8 rounded-full"
+        />
+        <p class="pl-2 font-extrabold">{{ author }}</p>
+      </article>
+    </section>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { NuxtImg } from '#components'
 import articleImage from 'public/images/illustration-article.svg'
+import BlogCard from '@/components/BlogCard.vue'
 const isHovered = ref(false)
 </script>
 
@@ -8,56 +9,14 @@ const isHovered = ref(false)
   <div
     class="bg-primary w-full min-h-screen font-figtree flex items-center justify-center p-4 relative"
   >
-    <div
-      class="relative w-full max-w-card-lg-w xs:max-w-card-sm-w"
-      @mouseenter="isHovered = true"
-      @mouseleave="isHovered = false"
-    >
-      <!-- Shadow -->
-      <div
-        :class="[
-          'absolute w-full h-full bg-dark rounded-xl left-2 top-2 z-0 transition-all duration-300',
-          isHovered ? 'translate-x-1 translate-y-1 scale-102 opacity-80' : '',
-        ]"
-      ></div>
-
-      <!-- Blog Card -->
-      <section
-        class="relative bg-white w-full rounded-xl shadow-lg p-6 z-10 border border-dark group transition-shadow duration-300 group-hover:shadow-2xl"
-      >
-        <article class="flex flex-col items-center">
-          <NuxtImg
-            :src="articleImage"
-            alt="Image with black and yellow shapes"
-            class="w-img-lg-w h-img-lg-h xs:w-img-sm-w xs:h-img-sm-h object-cover mb-6 rounded-lg"
-          />
-        </article>
-        <span
-          class="bg-primary text-dark px-3 py-1 rounded-md text-sm font-semibold"
-        >
-          Learning
-        </span>
-        <p class="text-sm py-2">Publish 21 Dec 2023</p>
-        <article>
-          <h1
-            class="text-lg font-extrabold cursor-pointer group-hover:text-primary transition-colors duration-75"
-          >
-            HTML & CSS foundations
-          </h1>
-          <p class="text-sm text-gray py-2">
-            These languages are the backbone of every website, defining
-            structure, content, and presentation.
-          </p>
-        </article>
-        <article class="flex items-end">
-          <img
-            src="/public/images/greg-avatar.webp"
-            alt="avatar of Greg"
-            class="size-8"
-          />
-          <p class="pl-2 font-extrabold">Greg Hooper</p>
-        </article>
-      </section>
-    </div>
+    <BlogCard
+      title="HTML & CSS foundations"
+      category="Learning"
+      date="Published 21 Dec 2023"
+      description="These languages are the backbone of every website, defining structure, content, and presentation."
+      imageSrc="/images/illustration-article.svg"
+      avatarSrc="/images/greg-avatar.webp"
+      author="Greg Hooper"
+    />
   </div>
 </template>
