da9ed399d02e333282315b2d83d77bb5331e1b58
YasmineSan <yasmine-sanchez@proton.me>
2025-02-17 16:14:57 +0100

    Feat: update nuxt.config.ts with SEO enhancements and language attributes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,14 +8,44 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/css/tailwind.css'],
   devtools: { enabled: true },
+
   nitro: {
     output: {
       publicDir: '.output/public',
     },
   },
+
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'Blog Preview Card | Frontend Mentor Challenge',
+      meta: [
+        {
+          name: 'description',
+          content: 'A blog preview card built with Nuxt and TailwindCSS',
+        },
+        {
+          name: 'robots',
+          content: 'index, follow',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+      ],
       link: [
+        {
+          rel: 'preload',
+          as: 'fetch',
+          href: '/_nuxt/builds/meta/f108dbf9.json',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'canonical',
+          href: 'https://blog-card-preview-challenge.netlify.app',
+        },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Figtree:wght@500;800&display=swap',
